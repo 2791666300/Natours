c5f9b8c8e32902151093e448b332ca5b392679c6
曲木车博 <2791666300@qq.com>
2022-08-19 13:10:55 +0800

    为部署准备的应用

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieParser = require('cookie-parser')
+const compression = require('compression')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -74,6 +75,7 @@ app.use(hpp({
       ]
 }))
 
+app.use(compression())
 // 提供静态文件 中间件
 app.use(express.static('./public'))
 
